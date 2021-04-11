8212949e6ef449e202031e10b72aa8c2d49a66a2
Vincent Westenberg <vincent.westenberg@posteo.de>
2021-04-11 12:24:02 +0200

    Create empty window

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/* Copyright 2021 Vincent Westenberg
+ * This file is part of Chicken Draw.
+ *
+ * Chicken Draw is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Chicken Draw is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Chicken Draw.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QIcon>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    app.setWindowIcon(QIcon(":/icons/logo.svg"));
+
+    engine.load("qrc:/MainWindow.qml");
+
+    return app.exec();
+}
